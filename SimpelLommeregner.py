e178a1c71f751159f1bab93c86127941e734655a
Marinus <marinus@triers.net>
2023-02-21 14:59:48 +0100

    Initial commit

---
+++ SimpelLommeregner.py
@@ -0,0 +1,40 @@
+Input = input("Lommeregner ")
+
+def funcVar1(Input):
+    if "+" in Input:
+        List = Input.split("+")
+        Var1 = List[0]
+    elif "-" in Input:
+        List = Input.split("-")
+        Var1 = List[0]
+    elif "*" in Input:
+        List = Input.split("*")
+        Var1 = List[0]
+    elif "/" in Input:
+        List = Input.split("/")
+        Var1 = List[0]
+    return float(Var1)
+
+def funcVar2(Input):
+    if "+" in Input:
+        List = Input.split("+")
+        Var2 = List[1]
+    elif "-" in Input:
+        List = Input.split("-")
+        Var2 = List[1]
+    elif "*" in Input:
+        List = Input.split("*")
+        Var2 = List[1]
+    elif "/" in Input:
+        List = Input.split("/")
+        Var2 = List[1]
+    return float(Var2)
+
+if "+" in Input:
+    print(funcVar1(Input)+funcVar2(Input))
+elif "-" in Input:
+    print(funcVar1(Input)-funcVar2(Input))
+elif "*" in Input:
+    print(funcVar1(Input)*funcVar2(Input))
+elif "/" in Input:
+    print(funcVar1(Input)/funcVar2(Input))
