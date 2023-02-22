4e972c5730eed6ab6fb34afd4dc6c29a8e353c1a
Marinus <marinus@triers.net>
2023-02-22 09:52:54 +0100

    Function checks a sigle digit string for +,-,* and / and returns a false if it finds one if not it returns a true

--- SimpelLommeregner.py
+++ SimpelLommeregner.py
@@ -1,6 +1,24 @@
-Input = input("Lommeregner ")
-Input.replace(" ","")
+#Input = input("Lommeregner ")
+#Input.replace(" ","")
 
+# findToken("-10+4*4") -> ("-", "10+4*4")
+# findToken("+10+4*4") -> ("+", "10+4*4")
+# findToken("4") -> ("4", "")
+# findToken("10+4*4") -> ("10", "+4*4")
+# findToken("") -> ("", False)
+
+def isNumber(strToTest):
+    Math = ["+","-","*","/"]
+    mathNum = 0
+    for mathNum in range(4):
+        if Math[mathNum] in strToTest:
+            return False
+    return True
+
+def findToken(Input):
+    pass
+
+"""
 def funcVar1(Input):
     if "+" in Input:
         List = Input.split("+")
@@ -31,7 +49,6 @@ def funcVar2(Input):
         Var2 = List[1]
     return float(Var2)
 
-
 if "+" in Input:
     print(funcVar1(Input)+funcVar2(Input))
 elif "-" in Input:
@@ -40,3 +57,11 @@ elif "*" in Input:
     print(funcVar1(Input)*funcVar2(Input))
 elif "/" in Input:
     print(funcVar1(Input)/funcVar2(Input))
+"""
+"""
+if findToken("-10+4*4") == ("-", "10+4*4") and findToken("+10+4*4") == ("+", "10+4*4") and findToken("4") == ("4", "") and findToken("10+4*4") == ("10", "+4*4") and findToken("") == ("", False):
+    print("det virkede!")
+else:
+    print("det virkede ikke")
+"""
+print(isNumber("9"))
